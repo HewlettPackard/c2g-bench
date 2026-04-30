345085c007ccf5abbe9bf27914582e5d9b25b377
vineet-gundecha <vineet.gundecha@hpe.com>
2026-04-30 04:51:25 -0700

    Rename file

--- c2g_env/train_envs/__init__.py
+++ c2g_env/train_envs/__init__.py
@@ -1,4 +1,4 @@
 from c2g_env.train_envs.rule_macro_wrapped import RuleMacroWrappedEnv
-from c2g_env.train_envs.fixed_cooling_wrapper import FixedActionWrapper
+from c2g_env.train_envs.fixed_action_wrapper import FixedActionWrapper
 
 __all__ = ["RuleMacroWrappedEnv", "FixedActionWrapper"]
